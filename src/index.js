1eca84298a920ca036f58970f2ad3d3efcfcbc17
Christina Lambiase <christina.lambiase@gmail.com>
2023-06-15 18:06:27 -0700

    complete lab

--- src/index.js
+++ src/index.js
@@ -1,5 +1,23 @@
 const init = () => {
   
+const inputForm = document.querySelector('form')
+inputForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const input = document.querySelector("#searchByID")
+
+    fetch(`http://localhost:3000/movies/${input.value}`)
+    .then(res => res.json())
+    .then(data => {
+        const title = document.querySelector('h4')
+        const summary = document.querySelector('p')
+    
+        title.innerText = data.title
+        summary.innerText = data.summary
+    
+    })
+
+})
+
 }
 
 document.addEventListener('DOMContentLoaded', init);
